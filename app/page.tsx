3614a5e92c442e37869cd2f9dc69c7f9a0eb0ac0
Michael Frieze <michaelfrieze@icloud.com>
2023-12-12 19:25:19 -0500

    feat: add authentication with Clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+export default function Page() {
   return (
-    <Button variant={"destructive"} size={"lg"}>
-      Click Me
-    </Button>
+    <div className="flex flex-col gap-y-4">
+      <h1>Dashboard</h1>
+      <UserButton afterSignOutUrl="/" />
+    </div>
   );
 }

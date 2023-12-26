1e5ffdddf486eacbe53b4c1221610eccce3383ac
Michael Frieze <michaelfrieze@icloud.com>
2023-12-26 13:43:45 -0500

    feat: add recommended list

--- app/(browse)/_components/sidebar/index.tsx
+++ app/(browse)/_components/sidebar/index.tsx
@@ -1,11 +1,18 @@
+import { getRecommended } from "@/lib/recommended-service";
+
 import { Wrapper } from "./wrapper";
 import { Toggle, ToggleSkeleton } from "./toggle";
+import { Recommended, RecommendedSkeleton } from "./recommended";
 
 export const Sidebar = async () => {
+  const recommended = await getRecommended();
+
   return (
     <Wrapper>
       <Toggle />
-      <div className="space-y-4 pt-4 lg:pt-0">Sidebar</div>
+      <div className="space-y-4 pt-4 lg:pt-0">
+        <Recommended data={recommended} />
+      </div>
     </Wrapper>
   );
 };
@@ -14,6 +21,7 @@ export const SidebarSkeleton = () => {
   return (
     <aside className="fixed left-0 z-50 flex h-full w-[70px] flex-col border-r border-[#2D2E35] bg-background lg:w-60">
       <ToggleSkeleton />
+      <RecommendedSkeleton />
     </aside>
   );
 };

6ada12e5e6f5750d8b3120af6aaf0227bd8d11df
Michael Frieze <michaelfrieze@icloud.com>
2024-01-14 23:50:03 -0500

    feat: add follow service

--- app/(browse)/[username]/page.tsx
+++ app/(browse)/[username]/page.tsx
@@ -1,3 +1,9 @@
+import { notFound } from "next/navigation";
+
+import { getUserByUsername } from "@/lib/user-service";
+import { isFollowingUser } from "@/lib/follow-service";
+import { Actions } from "./_components/actions";
+
 interface UserPageProps {
   params: {
     username: string;
@@ -5,9 +11,20 @@ interface UserPageProps {
 }
 
 const UserPage = async ({ params }: UserPageProps) => {
+  const user = await getUserByUsername(params.username);
+
+  if (!user) {
+    notFound();
+  }
+
+  const isFollowing = await isFollowingUser(user.id);
+
   return (
-    <div>
-      <h1>{params.username}</h1>
+    <div className="flex flex-col gap-y-4">
+      <p>username: {user.username}</p>
+      <p>user ID: {user.id}</p>
+      <p>is following: {`${isFollowing}`}</p>
+      <Actions isFollowing={isFollowing} userId={user.id} />
     </div>
   );
 };

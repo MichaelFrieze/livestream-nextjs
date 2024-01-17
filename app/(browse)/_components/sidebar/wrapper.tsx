f07d7cabc262f1fe19e8fe50da8df73facf8b0e5
Michael Frieze <michaelfrieze@icloud.com>
2024-01-16 21:42:33 -0500

    feat: add follow list

--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -6,6 +6,7 @@ import { cn } from "@/lib/utils";
 import { useSidebar } from "@/store/use-sidebar";
 
 import { ToggleSkeleton } from "./toggle";
+import { FollowingSkeleton } from "./following";
 import { RecommendedSkeleton } from "./recommended";
 
 interface WrapperProps {
@@ -16,16 +17,11 @@ export const Wrapper = ({ children }: WrapperProps) => {
   const isClient = useIsClient();
   const { collapsed } = useSidebar((state) => state);
 
-  // This is not needed anymore, as we are using useIsClient from usehooks-ts
-  // const [isClient, setIsClient] = useState(false);
-  // useEffect(() => {
-  //   setIsClient(true);
-  // }, []);
-
   if (!isClient) {
     return (
       <aside className="fixed left-0 z-50 flex h-full w-[70px] flex-col border-r border-[#2D2E35] bg-background lg:w-60">
         <ToggleSkeleton />
+        <FollowingSkeleton />
         <RecommendedSkeleton />
       </aside>
     );

d85791cdf7143c91e69d7e8016673359b4330e2d
Michael Frieze <michaelfrieze@icloud.com>
2024-01-18 21:15:46 -0500

    feat: add block service

--- app/(browse)/[username]/page.tsx
+++ app/(browse)/[username]/page.tsx
@@ -2,6 +2,7 @@ import { notFound } from "next/navigation";
 
 import { getUserByUsername } from "@/lib/user-service";
 import { isFollowingUser } from "@/lib/follow-service";
+import { isBlockedByUser } from "@/lib/block-service";
 import { Actions } from "./_components/actions";
 
 interface UserPageProps {
@@ -18,12 +19,14 @@ const UserPage = async ({ params }: UserPageProps) => {
   }
 
   const isFollowing = await isFollowingUser(user.id);
+  const isBlocked = await isBlockedByUser(user.id);
 
   return (
     <div className="flex flex-col gap-y-4">
       <p>username: {user.username}</p>
       <p>user ID: {user.id}</p>
       <p>is following: {`${isFollowing}`}</p>
+      <p>is blocked by this user: {`${isBlocked}`}</p>
       <Actions isFollowing={isFollowing} userId={user.id} />
     </div>
   );

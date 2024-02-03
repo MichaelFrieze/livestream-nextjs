cf99c7b90e7c913f9bf5fdc812501025a43b5fd3
Michael Frieze <michaelfrieze@icloud.com>
2024-02-02 20:29:40 -0500

    feat: add viewer token and stream-player component

--- app/(dashboard)/u/[username]/(home)/page.tsx
+++ app/(dashboard)/u/[username]/(home)/page.tsx
@@ -1,6 +1,7 @@
 import { currentUser } from "@clerk/nextjs";
 
 import { getUserByUsername } from "@/lib/user-service";
+import { StreamPlayer } from "@/components/stream-player";
 
 interface CreatorPageProps {
   params: {
@@ -12,11 +13,13 @@ const CreatorPage = async ({ params }: CreatorPageProps) => {
   const externalUser = await currentUser();
   const user = await getUserByUsername(params.username);
 
+  if (!user || user.externalUserId !== externalUser?.id || !user.stream) {
+    throw new Error("Unauthorized");
+  }
+
   return (
     <div className="h-full">
-      <h1>Creator page</h1>
-      <p>User: {user?.username}</p>
-      <p>External User: {externalUser?.username}</p>
+      <StreamPlayer user={user} stream={user.stream} isFollowing />
     </div>
   );
 };
---
+++ components/stream-player.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { useViewerToken } from "@/hooks/use-viewer-token";
+
+type CustomStream = {
+  id: string;
+  isChatEnabled: boolean;
+  isChatDelayed: boolean;
+  isChatFollowersOnly: boolean;
+  isLive: boolean;
+  thumbnailUrl: string | null;
+  name: string;
+};
+
+type CustomUser = {
+  id: string;
+  username: string;
+  bio: string | null;
+  stream: CustomStream | null;
+  imageUrl: string;
+  _count: { followedBy: number };
+};
+
+interface StreamPlayerProps {
+  user: CustomUser;
+  stream: CustomStream;
+  isFollowing: boolean;
+}
+
+export const StreamPlayer = ({
+  user,
+  stream,
+  isFollowing,
+}: StreamPlayerProps) => {
+  const { token, name, identity } = useViewerToken(user.id);
+
+  if (!token || !name || !identity) {
+    return <div>Cannot watch the steram</div>;
+  }
+
+  return (
+    <div>
+      <h1>Allowed to watch the stream</h1>
+    </div>
+  );
+};
